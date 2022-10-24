a74dacef9bc7be55409596d3a2e4631a64c98b8f
Hanna Saurytskaya <anna.kassy.s@gmail.com>
2022-10-24 23:21:59 +0300

    Add task 1

---
+++ src/home_work_1/Main1.java
@@ -0,0 +1,31 @@
+package home_work_1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main1 {
+    public static void main(String[] args) {
+        int numberOne = 0, numberTwo = 0;
+        try(BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            System.out.println("Введите первое число:");
+            numberOne = Integer.parseInt(reader.readLine()); //5
+            System.out.println("Введите второе число:");
+            numberTwo = Integer.parseInt(reader.readLine()); //9
+        } catch(Exception e) {
+            System.out.println("Неверно введены значения.");
+            return;
+        }
+        int resultAnd = numberOne & numberTwo; //00000101 & 00001001 = 00000001
+        int resultOr = numberOne | numberTwo; //00000101 & 00001001 = 00001101
+        System.out.printf("Результат, полученный при использовании побитового И для чисел %d и %d, равен %d.\n",
+                            numberOne, numberTwo, resultAnd);
+        System.out.printf("Результат, полученный при использовании побитового ИЛИ для чисел %d и %d, равен %d.",
+                numberOne, numberTwo, resultOr);
+    }
+}
+/*
+Побитовые операции не предназначены для вещественных чисел.
+При записи команды "System.out.println(~42.5);" возникает ошибка "bad operand type double for unary operator '~'"
+еще на этапе сборки проекта.
+ */
