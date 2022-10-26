c2a3771e9189ff6ecde4336ef770eb9cd6ab4143
Hanna Saurytskaya <anna.kassy.s@gmail.com>
2022-10-26 19:08:15 +0300

    Add task 5

---
+++ src/home_work_1/Main5.java
@@ -0,0 +1,20 @@
+package home_work_1;
+
+public class Main5 {
+    public static void main(String[] args) {
+        boolean isWeekday = true;
+        boolean isVacation = false;
+
+        if(sleepIn(isWeekday, isVacation)) {
+            System.out.println("Можно спать дальше.");
+        }
+        else {
+            System.out.println("Пора идти на работу.");
+        }
+    }
+
+    static boolean sleepIn(boolean weekday,  boolean vacation) {
+        boolean sleepy = !weekday || vacation;
+        return sleepy;
+    }
+}
